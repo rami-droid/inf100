43cab5c06eb650819eef44638381f127ed347677
rami <rami.r.abbadi@gmail.com>
2025-08-25 20:14:48 +0200

    fix bug

--- lab1/innskudd.py
+++ lab1/innskudd.py
@@ -7,5 +7,8 @@ data = json.loads(content)
 
 name = data['personalia']['navn']
 value = data['konti'][0]['saldo']
+value2 = data['konti'][1]['saldo']
 
-print(f"{name} har {value} kroner i innskudd")
+sum_value = value + value2
+
+print(f"{name} har {sum_value} kroner i innskudd")
