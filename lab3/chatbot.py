4a2b7893f5d93ea4f5c6c22d4c398675920830e0
rami <billdoors90@gmail.com>
2025-09-15 11:34:09 +0200

    add from lab3

---
+++ lab3/chatbot.py
@@ -0,0 +1,11 @@
+user_input = str(input("Hi! Do you want to talk to me?\n")).strip().lower()
+
+running = True
+
+while running:
+    if user_input != "no":
+        print("That's cool \n")
+        user_input = str(input("What do you want to talk about?\n")).strip().lower()
+    else:
+        print("Alright, bye!")
+        running = False
