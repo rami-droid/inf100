2b3c8f15c78cb651a425f8274e7ed951dc0b72c7
rami <rami.r.abbadi@gmail.com>
2025-09-28 17:18:41 +0200

    fix for tests

--- lab3/vowels.py
+++ lab3/vowels.py
@@ -1,11 +1,18 @@
 def vowels(strings):
     vowels = ["a", "e", "i", "o", "u"]
+    if isinstance(strings, str):
+        for char in strings.lower():
+            if char in vowels:
+                count += 1
+        print(f'"{text}": {count}')
+        return count
     for text in strings:
         count = 0
         for char in text.lower():
             if char in vowels:
                 count += 1
         print(f'"{text}": {count}')
+        return count
 if __name__ == "__main__":
     from pathlib import Path
     import json
