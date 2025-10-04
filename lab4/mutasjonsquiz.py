02207208249f179743707c26b60399f0be19296f
rami <billdoors90@gmail.com>
2025-10-04 16:48:04 +0200

    mutasjon quiz

---
+++ lab4/mutasjonsquiz.py
@@ -0,0 +1,20 @@
+funA_is_destructive = True  # True eller False
+funA_description = (
+    "Funksjonen fjerner direkte fra listen."  # Skriv din forklaring inne i strengen
+)
+
+
+funB_is_destructive = False
+funB_description = "returnerer ny liste som ikke endrer originale"
+
+funC_is_destructive = True
+funC_description = "Endrer verdiene til arrayen A"
+
+funD_is_destructive = False
+funD_description = "returnerer ny liste, A blir brukt for iterasjon"
+
+funE_is_destructive = True
+funE_description = "fjerner siste elementet fra a i loopen"
+
+funF_is_destructive = True
+funF_description = "endrer listen"
